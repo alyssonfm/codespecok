9b33ae5bc05b7909092ad3b1fe2f2cb77b9dbebe
ddspog <ddspog@gmail.com>
2015-04-15 15:47:35 -0300

    Updating Commons package with dennis branch
    I've just added the Utils.cs file, since the rest is equal.

---
+++ Commons/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commons
+{
+    public static class Utils
+    {
+        public static bool IsAny<T>(this IEnumerable<T> data)
+        {
+            return data != null && data.Any<T>();
+        }
+
+        public static List<T> GetList<T>()
+        {
+            return new List<T>((T[])Enum.GetValues(typeof(T)));
+        }
+    }
+}
