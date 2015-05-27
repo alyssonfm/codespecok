d4fef2ba5ca33225cdcf020a57ebfbbb3137ad0f
Dênnis Dantas <ddspog@gmail.com>
2015-05-27 12:57:50 -0300

    Elaborate function to walk on Methods
    I've started implemented the function necessary to walk on Methods. I've
    just treated the cases where the line will be a simple expression of
    assignment, then excluding variables names, integer values from the
    analysis.
    
    I still need to implement the call for WalkOn(function.location).

---
+++ CategorizeModule/Tests/StaticAnalysisTesting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using Commons;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CategorizeModule.Tests
+{
+    class StaticAnalysisTesting
+    {
+        public void TestNonconformancesLikelyCause()
+        {
+            MethodDeclarationSyntax test;
+            List<ReachableMethod> methodsAvailable;
+
+            WalkOnTest(test, methodsAvailable);
+        }
+    }
+}
---
+++ CategorizeModule/Walker.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using Commons;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CategorizeModule
+{
+    class Walker
+    {
+        public static Score WalkOnTest(MethodDeclarationSyntax test, List<ReachableMethod> methodsAvailable)
+        {
+            SyntaxList<StatementSyntax> block = test.Body.Statements;
+            
+            foreach (StatementSyntax s in block)
+            {
+                if (s is ExpressionStatementSyntax)
+                {
+                    ExpressionStatementSyntax e = (ExpressionStatementSyntax) s;
+                    StepOnFunctions(e.Expression);
+                }
+            }
+
+            return null;
+        }
+
+        private static void StepOnFunctions(ExpressionSyntax line)
+        {
+            if (line is ParenthesizedExpressionSyntax)
+            {
+                StepOnFunctions(((ParenthesizedExpressionSyntax)line).Expression);
+            }
+            else if (line is BinaryExpressionSyntax)
+            {
+                StepOnFunctions(((BinaryExpressionSyntax)line).Left);
+                StepOnFunctions(((BinaryExpressionSyntax)line).Right);
+            }
+            else if (line is PrefixUnaryExpressionSyntax)
+            {
+                StepOnFunctions(((PrefixUnaryExpressionSyntax)line).Operand);
+            }
+            else if (line is PostfixUnaryExpressionSyntax)
+            {
+                StepOnFunctions(((PostfixUnaryExpressionSyntax)line).Operand);
+            }
+            else if (line is AssignmentExpressionSyntax)
+            {
+                StepOnFunctions(((AssignmentExpressionSyntax)line).Left);
+            }
+            else if (line is IdentifierNameSyntax)
+            {
+                return;
+            }
+            else if (line is MemberAccessExpressionSyntax)
+            {
+                return;
+            }
+            else if (line is InvocationExpressionSyntax)
+            {
+                InvocationExpressionSyntax inv = (InvocationExpressionSyntax)line;
+                foreach(ArgumentSyntax a in inv.ArgumentList.Arguments)
+                {
+                    StepOnFunctions(a.Expression);
+                }
+                inv.ArgumentList.Arguments
+            }
+        }
+
+    }
+}
